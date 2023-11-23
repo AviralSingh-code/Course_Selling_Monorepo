09e73507950b1f225e369ad1d7e6425e7dad7e33
Aviral Singh <kalhansaviral@gmail.com>
2023-11-23 10:43:41 +0530

    Added appbar

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { RecoilRoot } from 'recoil';
-
+import { RecoilRoot, useRecoilValue } from 'recoil';
+import { Appbar } from "ui/Appbar";
+import { isUserLoading } from 'store';
 export default function App({ Component, pageProps }: AppProps) {
+  
   return <RecoilRoot>
+    <HeaderComponent></HeaderComponent>
     <Component {...pageProps} />
   </RecoilRoot>
+}
+
+function HeaderComponent()
+{
+  const userState = useRecoilValue(isUserLoading);
+  return ( <Appbar title={"EduWaveX"} userState={userState}></Appbar> );
 }
